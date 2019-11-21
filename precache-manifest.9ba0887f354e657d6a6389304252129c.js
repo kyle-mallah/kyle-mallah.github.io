01089ac74c40b269bd680351fe76b23f4a67443d
Kyle <kylemallah@hotmail.com>
2019-11-21 15:45:50 -0500

    Updates

---
+++ precache-manifest.9ba0887f354e657d6a6389304252129c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b9a264d42309de345404356747b5eb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce3b370d6c869fa7d921",
+    "url": "/static/css/main.04265a12.chunk.css"
+  },
+  {
+    "revision": "6ea1fab880dcda4c1ee4",
+    "url": "/static/js/2.3b5819db.chunk.js"
+  },
+  {
+    "revision": "ce3b370d6c869fa7d921",
+    "url": "/static/js/main.3defe87c.chunk.js"
+  },
+  {
+    "revision": "6e4aa46fe87f1f8303b7",
+    "url": "/static/js/runtime-main.303736f4.js"
+  }
+]);
